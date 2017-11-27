b7b1f0ee4bbdb024091561f421beb554409f30d2
James <james.sohuang@hotmail.com>
2017-11-27 14:40:15 -0500

    Styling changes and updates to html

--- popup.js
+++ popup.js
@@ -1,9 +1,11 @@
+const API_KEY = 'FY5QTR0KF1HCBMZA';
+
 $(document).ready(() => {
 	// Get the current active tab name
 	let activeTab = getActiveTab();
 	loadCurrentTab(activeTab);
 
-	$('.nav-item').click(function () {
+	$('.nav-item').click(function() {
 		$('.nav-item').removeClass('active');
 		$(this).addClass('active');
 		activeTab = getActiveTab();
