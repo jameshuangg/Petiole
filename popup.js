a56d9783d6c99e4b7e64b2e496406a473ece707f
James <james.sohuang@hotmail.com>
2017-11-27 14:41:40 -0500

    Added all the neccessary functions gave them them async/await capabilities

--- popup.js
+++ popup.js
@@ -13,16 +13,91 @@ $(document).ready(() => {
 	});
 });
 
+async function addStock(stockSymbol) {
+	let tempStocks = [];
+	let currentStocks = await getCurrentStocks();
+	if (!currentStocks) { return; }
+	currentStocks.forEach(function(stock) {
+		tempStocks.push(stock);
+	});
+	if (!tempStocks.includes(stockSymbol)) {
+		tempStocks.push(stockSymbol);
+	}
+	return new Promise(function(resolve, reject) {
+		resolve(chrome.storage.local.set({ stocks: tempStocks }));
+	});
+}
+
+async function removeStock(stockSymbol) {
+	let tempStocks = [];
+	let currentStocks = await getCurrentStocks();
+	if (!currentStocks) { return; }
+	currentStocks.forEach(function(stock) {
+		if (stock === stockSymbol) { return; }
+		tempStocks.push(stock);
+	});
+	return new Promise(function(resolve, reject) {
+		resolve(chrome.storage.local.set({ stocks: tempStocks }));
+	});
+}
+
+async function updateValues() {
+	let currentStocks = await getCurrentStocks();
+	if (!currentStocks) { return; }
+	$('#stock-content tbody').empty();
+	currentStocks.forEach(function(symbol) {
+		let quote = getStockValue(symbol);
+		// Create a row
+		let row = $('<tr class="gain-text"></tr>');
+		row.append($('<td></td>').text(symbol));
+		row.append($('<td></td>').text(quote.open));
+		row.append($('<td></td>').text(quote.high));
+		row.append($('<td></td>').text(quote.low));
+		$('#stock-content tbody').append(row);
+	});
+}
+
 function loadCurrentTab(tabName) {
 	if (tabName === 'Stocks') {
-		$('#content').load('stock.html');
+		$('#content').load('stock.html', function() {
+
+		});
 	} else if (tabName === 'Currency') {
-		$('#content').load('currency.html');
+		$('#content').load('currency.html', function() {
+
+		});
 	} else if (tabName === 'Analysis') {
-		$('#content').load('analysis.html');
+		$('#content').load('analysis.html', function () {
+
+		});
 	}
 }
 
 function getActiveTab() {
 	return $('nav').find('.active').text();
+}
+
+function getStockValue(symbol) {
+	return new Promise(function(resolve, reject) {
+		let xhr = new XMLHttpRequest();
+		xhr.onreadystatechange = function () {
+			if(xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+				let jsonResponse = JSON.parse(xhr.response);
+				// Gets the most recent quote
+				let lastRefresh = jsonResponse['Meta Data']['3. Last Refreshed'];
+				let quoteObject = jsonResponse['Time Series (1min)'];
+				resolve(quoteObject);
+			}
+		}
+		xhr.open('GET', `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${symbol}&interval=1min&outputSize=compact&datatype=json&apikey=${API_KEY}`);
+		xhr.send();
+	});
+}
+
+function getCurrentStocks() {
+	return new Promise(function(resolve, reject) {
+		chrome.storage.local.get('stocks', function(storage) {
+			resolve(storage.stocks);
+		});
+	});
 }
